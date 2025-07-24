1e44ec68b9dcbac91b617e678700da5619167cf9
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-07-24 16:49:12 +0530

    tailwind.css file updated

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            'brand-red': '#D32F2F',
+            'light-red': '#FFCDD2',
+        },
+    },
   },
   plugins: [],
 }
