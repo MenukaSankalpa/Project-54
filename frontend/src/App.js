5cd70defdf12997ebe507ce22d4f7b46f60061e2
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-19 15:34:19 +0530

    updated all files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
-import Login from "./pages/Login"; // build later
-import TaskForm from "./pages/TaskForm"; // build later
-import Users from "./pages/Users"; // build later
-import { useState } from "react";
+import Login from './pages/Login';
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  const handleLogout = () => setUser(null);
-
-  return (
-    <Router>
-      <Navbar user={user} onLogout={handleLogout} />
-      <Routes>
-        <Route path="/login" element={<Login setUser={setUser} />} />
-        <Route path="/dashboard" element={<Dashboard currentUser={user || { role: "employee" }} />} />
-        <Route path="/task/create" element={<TaskForm />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
-    </Router>
-  );
+  return <Login />;
 }
 
 export default App;
