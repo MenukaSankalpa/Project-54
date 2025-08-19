cb000d656776e878bc1cd8be83607da2fc75d4c9
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-19 14:39:35 +0530

    db.js added

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root123',
+  database: 'task_manager'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('Connected to MySQL');
+});
+
+module.exports = db;
+
