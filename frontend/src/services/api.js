3b5b6ee4687a2c27c6d3d52aeef27be2dc49b4df
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-19 13:56:36 +0530

    api.js added MySql routes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,21 @@
-import axios from 'axios';
+//this rout for XAMP uses
+/*import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'http://localhost:8000/api',
     withCredentials: true,
 });
 
+export default api;*/
+
+
+//this rout for MySql uses
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api",
+  withCredentials: true,
+});
+
 export default api;
+
