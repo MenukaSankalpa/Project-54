ba64872f2ed2ceb0ec33aed0b66fadacb648663a
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-08-20 13:53:08 +0530

    routs added

---
+++ task-management-system/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
+  );
+}
